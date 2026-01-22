4107e0203accbcb07184a2e800c5aa0e979b925e
vuongducanh <anhvuong730@gmail.com>
2026-01-22 21:52:00 +0700

    add common getAuthenticatedUser

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -1,41 +1,13 @@
-import { decryptToken } from "@/lib/auth/jwt";
-import prisma from "@/lib/prisma";
+import { getAuthenticatedUser } from "@/lib/auth/jwt";
 import { ApiResponse } from "@/types/responses";
 
 export async function GET(req: Request) {
   try {
-    const authHeader = req.headers.get("authorization");
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return ApiResponse.unauthorized();
-    }
-
-    const accessToken = authHeader.replace("Bearer ", "");
-
-    const payload = await decryptToken(accessToken);
-
-    if (!payload) {
-      return ApiResponse.unauthorized("Invalid or expired token");
-    }
-
-    const userId = payload.userId as number;
-
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      select: {
-        id: true,
-        email: true,
-        fullName: true,
-        createdAt: true,
-      },
-    });
-
-    if (!user) {
-      return ApiResponse.notFound("User not found");
-    }
+    const user = await getAuthenticatedUser(req);
 
     return ApiResponse.successResponse(user);
   } catch (err) {
-    return ApiResponse.unauthorized("Invalid or expired token");
+    // err is already ApiResponse
+    return err as Response;
   }
 }
--- src/lib/auth/jwt.ts
+++ src/lib/auth/jwt.ts
@@ -1,6 +1,8 @@
 import { AUTH_EXPIRES } from "@/config/auth.config";
 import { SignJWT, jwtVerify } from "jose";
 import "server-only";
+import prisma from "@/lib/prisma";
+import { ApiResponse } from "@/types/responses";
 
 const secretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(secretKey);
@@ -35,4 +37,38 @@ export async function decryptToken(session: string | undefined = "") {
     console.log("Failed to verify session");
     return null
   }
+}
+
+export async function getAuthenticatedUser(req: Request) {
+  const authHeader = req.headers.get("authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw ApiResponse.unauthorized();
+  }
+
+  const accessToken = authHeader.replace("Bearer ", "");
+
+  const payload = await decryptToken(accessToken);
+
+  if (!payload) {
+    throw ApiResponse.unauthorized("Invalid or expired token");
+  }
+
+  const userId = payload.userId as number;
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      email: true,
+      fullName: true,
+      createdAt: true,
+    },
+  });
+
+  if (!user) {
+    throw ApiResponse.notFound("User not found");
+  }
+
+  return user;
 }
